1b9f9ffb6b703418e972cca47a5873eaefffd3a4
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-06-15 00:38:44 +0200

    Change Contact.js and delete comments

--- components/Contact.js
+++ components/Contact.js
@@ -3,27 +3,26 @@ var Contact = React.createClass({
   propTypes: {
     item: React.PropTypes.object.isRequired,
   },
+
   render: function() {
     return (
-      // React.createElement('div',{className: 'contactItem'},
       <div className={'contactItem'}>
-        // React.createElement('img', {className: 'contactImage', src: this.props.item.photo}),
         <figure>
           <img className={'contactImage'} src={this.props.item.photo}></img>
         </figure>
-        // React.createElement('p', {className: 'contactLabel'}, 'Name: ' + this.props.item.firstName),
+
         <p className={'contactLabel'}>
           Name: {this.props.item.firstName}
         </p>
-        // React.createElement('p', {className: 'contactLabel'}, 'Surname: ' + this.props.item.lastName),
+
         <p className={'contactLabel'}>
           Surname: {this.props.item.lastName}
         </p>
-        // React.createElement('a', {className: 'contactEmail', href: "Email " + this.props.item.email}, 'Email: ' + this.props.item.email)
-        <a className={'contactEmail'} href={'Email ' + this.props.item.email}>
+
+        <a className={'contactEmail'} href={'Email: ' + this.props.item.email}>
           Email: {this.props.item.email}
         </a>
       </div>
     )
-  },
+  }
 });
